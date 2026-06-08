17e9d21b143528eb5b73f3370404cbe2147f9aee
nganchaubd123 <nganchau.bd123@gmail.com>
2026-06-08 20:34:31 +0700

    modify testcase

--- solution.java
+++ solution.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 import java.util.HashMap;
 
 public class solution {
-    public static void main(String[] args) {
+    public static void main(String[] args) { 
         Scanner sc = new Scanner(System.in);
 
         int n = sc.nextInt();
